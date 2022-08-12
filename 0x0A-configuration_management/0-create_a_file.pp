a45570609e3763e8160ec3dd5adb12efa09ecd1e
root <jesus4388>
2022-08-12 12:43:50 -0700

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #Using Puppet, create a file in /tmp.
 file {'/tmp/school':
   ensure  => 'file',
-  path    => '/tmp/school'
-  mode    => 'u=rwx,g=r,o=r',
+  path    => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
