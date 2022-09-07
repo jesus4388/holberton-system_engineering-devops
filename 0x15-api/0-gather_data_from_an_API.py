27a8c99468498a70f93baea165ba0b149a791efd
jesus4388 <jesushernandez1843@gmail.com>
2022-09-06 18:35:50 -0700

    Gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# a Python script that, using this REST API
+import requests
+import json
+from sys import argv
+
+
+if __name__ == "__main__":
+    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+            argv[1])
+    data = requests.get(user)
+    data = data.json()
+    _id = data.get("id")
+    name = data.get("name")
+
+    todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            argv[1])
+    todos = requests.get(todo)
+    todos = todos.json()
+    task = len(todos)
+    completed = 0
+    for _dict in todos:
+        if _dict.get('completed') is True:
+            completed += 1
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, completed, task))
+    for _dict in todos:
+        if _dict.get('completed') is True:
+            print("\t",_dict.get('title'))
