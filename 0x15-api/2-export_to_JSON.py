3a40e43f7e2f293717ff637629c15280acc328a5
jesus4388 <jesushernandez1843@gmail.com>
2022-09-06 19:39:53 -0700

    l

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,7 +8,6 @@ from sys import argv
 
 if __name__ == '__main__':
 
-
     response_user = requests.get(
             'https://jsonplaceholder.typicode.com/users/{}'.format(
                 argv[1]))
