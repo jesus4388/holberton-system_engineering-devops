5862cfea7b8b8a7c7b920180c57a97a10f0c7b61
jesus4388 <jesushernandez1843@gmail.com>
2022-09-06 19:07:20 -0700

    1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""extend your Python script to export data in the CSV format"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+            argv[1])
+    data = requests.get(url)
+    data = data.json()
+    name = data.get('username')
+    url1 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            argv[1])
+    posts = requests.get(url1)
+    posts = posts.json()
+
+    with open("{}".format(argv[1]) + ".csv", "w") as f:
+        result = csv.writer(f, dialect='unix')
+        for _dic in posts:
+            result.writerow(
+                    [argv[1],
+                        name,
+                        _dic.get('completed'),
+                        _dic.get('title')])
