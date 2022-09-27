122cd9c97a71cf0c5bfa84730f417271c18a17c0
jesus4388 <jesushernandez1843@gmail.com>
2022-09-27 17:05:00 +0000

    restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,7 @@ exec { 'change-limit':
     command  => "sed -i 's/-n 15/-n 7000/g' /etc/default/nginx",
     provider => shell,
 }
+exec { 'restart':
+    command  => '/etc/init.d/nginx restart',
+    provider => shell,
+}
